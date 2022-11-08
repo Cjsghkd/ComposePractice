93ea930b4bd1e9ae619c3cd23e312ff5a38d8b07
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2022-11-08 21:15:08 +0900

    Animated Circular Progress Bar

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -21,6 +21,8 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
@@ -35,6 +37,8 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -46,53 +50,63 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var sizeState by remember { mutableStateOf(200.dp) }
-            val size by animateDpAsState(
-                targetValue = sizeState,
-//                tween(
-//                    durationMillis = 3000,
-//                    delayMillis = 300,
-//                    easing = LinearOutSlowInEasing
-//                )
-
-//                spring(
-//                    Spring.DampingRatioHighBouncy
-//                )
-
-//                keyframes { // 간단한 애니메이션용 (왠만하면 keyframes 대신 tween 사용하기)
-//                    durationMillis = 5000
-//                    sizeState at 0 with LinearEasing
-//                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
-//                    sizeState * 2f at 5000
-//                }
-
-                tween(
-                    durationMillis = 1000
-                )
-            )
-            val infiniteTransition = rememberInfiniteTransition()
-            val color by infiniteTransition.animateColor(
-                initialValue = Color.Red,
-                targetValue = Color.Green,
-                animationSpec = infiniteRepeatable(
-                    tween(durationMillis = 2000),
-                    repeatMode = RepeatMode.Reverse
-                )
-            )
-            Box(modifier = Modifier
-                .size(size)
-                .background(color),
-            contentAlignment = Alignment.Center) {
-                Button(onClick = {
-                    sizeState += 50.dp
-                }) {
-                    Text("Increase Size")
-                }
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                CircularProgressBar(percentage = 0.8f, number = 100)
             }
         }
     }
 }
 
+@Composable
+fun CircularProgressBar(
+    percentage : Float,
+    number : Int,
+    fontSize : TextUnit = 28.sp,
+    radius : Dp = 50.dp,
+    color : Color = Color.Green,
+    strokeWidth : Dp = 8.dp,
+    animDuration : Int = 1000,
+    animDelay : Int = 0
+) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val curPercentage = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
+        )
+    )
+
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+    
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ) {
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                -90f,
+                360 * curPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        Text(
+            text = (curPercentage.value * number).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
 
 
 
