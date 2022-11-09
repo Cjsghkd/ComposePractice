b0d39421df18f6ab83672affcbd2d7295d631e6a
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2022-11-09 15:09:19 +0900

    Instagram Profile UI (TopBar, ProfileSection)

---
+++ app/src/main/java/com/example/composepractice/instagram/ProfileScreen.kt
@@ -0,0 +1,150 @@
+package com.example.composepractice.instagram
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ProfileScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopBar(name = "ParkSungHyun_official", modifier = Modifier
+            .padding(10.dp))
+        Spacer(modifier = Modifier.height(4.dp))
+        ProfileSection()
+    }
+}
+
+@Composable
+fun TopBar(
+    name: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Icon(
+            imageVector = Icons.Default.ArrowBack,
+            contentDescription = "Back",
+            tint = Color.Black,
+            modifier = Modifier.size(24.dp)
+        )
+        Text(
+            text = name,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        Icon(
+            painter = painterResource(id = com.example.composepractice.R.drawable.ic_bell),
+            contentDescription = "Back",
+            tint = Color.Black,
+            modifier = Modifier.size(24.dp)
+        )
+        Icon(
+            painter = painterResource(id = com.example.composepractice.R.drawable.ic_dotmenu),
+            contentDescription = "Back",
+            tint = Color.Black,
+            modifier = Modifier.size(20.dp)
+        )
+    }
+}
+
+@Composable
+fun ProfileSection(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        ) {
+            RoundImage(image = painterResource(id = com.example.composepractice.R.drawable.ggae_dal_umm), modifier = Modifier
+                .size(100.dp)
+                .weight(3f) // 전체 비율중 30%
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            StatSection(modifier = Modifier.weight(7f))
+        }
+    }
+}
+
+@Composable
+fun RoundImage(
+    image: Painter,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = modifier
+            .aspectRatio(1f, matchHeightConstraintsFirst = true) // 너비와 높이를 맞춘다
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = CircleShape
+            )
+            .padding(3.dp)
+            .clip(CircleShape)
+    )
+}
+
+@Composable
+fun StatSection(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier
+    ) {
+        ProfileStat(numberText = "601", text = "Posts")
+        ProfileStat(numberText = "100K", text = "Followers")
+        ProfileStat(numberText = "72", text = "Following")
+    }
+}
+
+@Composable
+fun ProfileStat(
+    numberText: String,
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Text(
+            text = numberText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = text
+        )
+    }
+}
