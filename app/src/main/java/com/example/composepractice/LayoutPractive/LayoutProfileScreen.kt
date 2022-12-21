b5b147d080081afc043b7236b8b750f6fb414655
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2022-12-21 15:48:15 +0900

    BasicLayout Practice (Align Your Body section)

--- app/src/main/java/com/example/composepractice/LayoutPractive/LayoutProfileScreen.kt
+++ app/src/main/java/com/example/composepractice/LayoutPractive/LayoutProfileScreen.kt
@@ -1,12 +1,20 @@
 package com.example.composepractice.LayoutPractive
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -31,5 +39,43 @@ fun SearchBar (
         modifier = modifier
             .fillMaxWidth()
             .heightIn(min = 56.dp)
+    )
+}
+
+@Composable
+fun AlignYourBodyElement(
+    @DrawableRes drawable: Int,
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(drawable),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.paddingFromBaseline(
+                top = 24.dp, bottom = 8.dp
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AlignYourBodyElementPreview() {
+    AlignYourBodyElement(
+        drawable = com.example.composepractice.R.drawable.ggae_dal_umm,
+        text = "Inversions",
+        modifier = Modifier.padding(8.dp)
     )
 }
